e3a19b95cd1e51da7e45a7fbdd523c297c0d82c7
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-11 11:17:47 +0300

    ft:just_finished_register_controller:backend

--- controllers/Register.js
+++ controllers/Register.js
@@ -1,1 +1,19 @@
 const User = require('../models/User')
+
+
+exports.Registration = async (req,res) => {
+    menu.startState({
+        run:() => {
+            const {phoneNumber} = menu.args
+            const user = await User.findOne({tel:phoneNumber})
+            const registerInstruction = `Welcome to mSACCO \nEnter your first name to register:`;
+            if (user){
+                menu.con(
+                    `Welcome back, ${user.username}! \nEnter your 4-digit PIN to continue`
+                ) 
+            }    else {
+                menu.con(registerInstruction)
+            }
+        }
+    })
+}
